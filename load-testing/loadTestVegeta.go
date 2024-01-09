9a27f6de964ee7042d8c7a2cfdafdb3e3a6aafb3
Vaibhav Ahuja <vaibhavahuja@outlook.com>
2024-01-09 09:15:51 +0530

    now we can easily run distributed tests

--- load-testing/loadTestVegeta.go
+++ load-testing/loadTestVegeta.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -10,12 +11,14 @@ import (
 )
 
 // Load tests for multiple bodies
+// now we can run distributed load tests as well by writing the results in result.bin
+// and combine later by vegeta report *.bin
 func main() {
 	rate := vegeta.Rate{
-		Freq: 10000,
+		Freq: 20,
 		Per:  1 * time.Second,
 	}
-	duration := 5 * time.Second
+	duration := 25 * time.Second
 	headers := http.Header{}
 	headers.Add("accept", "application/json")
 	headers.Add("Content-Type", "application/json")
@@ -36,12 +39,37 @@ func main() {
 	vegeta.KeepAlive(false)(attacker)
 	vegeta.Timeout(5 * time.Second)(attacker)
 
+	fileName := "results2.bin"
+	out, err := file(fileName, true)
+	if err != nil {
+		log.Fatalf("error opening %s: %s", fileName, err)
+	}
+	defer out.Close()
+
 	var metrics vegeta.Metrics
-	for res := range attacker.Attack(targeter, rate, duration, "myTest") {
+	enc := vegeta.NewEncoder(out)
+	results := attacker.Attack(targeter, rate, duration, "myTest")
+	for res := range results {
 		metrics.Add(res)
+		enc.Encode(res)
 	}
 	metrics.Close()
 
 	report := vegeta.NewTextReporter(&metrics)
 	report(os.Stdout)
+}
+
+// https://github.com/tsenart/vegeta/blob/master/attack.go
+func file(name string, create bool) (*os.File, error) {
+	switch name {
+	case "stdin":
+		return os.Stdin, nil
+	case "stdout":
+		return os.Stdout, nil
+	default:
+		if create {
+			return os.Create(name)
+		}
+		return os.Open(name)
+	}
 }
