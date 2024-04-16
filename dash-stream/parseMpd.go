322d407015434318b918d84fc36f62c8977595af
vaibhavahuja <vaibhavahuja@outlook.com>
2024-04-16 15:35:39 +0530

    understanding mpd file structure

--- dash-stream/parseMpd.go
+++ dash-stream/parseMpd.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/unki2aut/go-mpd"
+	"log"
+	"os"
 )
 
 // will do quick POC here
@@ -10,10 +12,21 @@ func main() {
 	//can use this to parse your file, but will have to write code to convert
 	// segmentTemplate to segments on own
 	mpd := new(mpd.MPD)
-	//mpd.Decode()
+	myFile, err := os.ReadFile("test.xml")
+	if err != nil {
+		log.Fatalf("error while opening file %s", err.Error())
+	}
+
+	err = mpd.Decode(myFile)
+	if err != nil {
+		log.Fatalf("error while decoding file %s", err.Error())
+	}
 	for _, period := range mpd.Period {
+		baseUrl := period.BaseURL[0].Value
+		fmt.Println(baseUrl)
 		//period.BaseURL
 		for _, adaptSet := range period.AdaptationSets {
+			//adaptSet.SegmentTemplate.Initialization
 			//fmt.Println("printing for ", *adaptSet.Codecs)
 			//adaptSet.SegmentTemplate.SegmentTimeline
 			for _, representation := range adaptSet.Representations {
